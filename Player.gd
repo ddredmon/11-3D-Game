4c368648012507922e0ee78b2523dba1a8ebc64a
ddredmon <ddredmon@iu.edu>
2019-11-19 22:15:49 -0500

    Does assignment

---
+++ Player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody
+
+var speed = 200
+var direction = Vector3()
+var gravity = -9.8
+var velocity = Vector3()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	direction = Vector3(0,0,0)
+	if Input.is_action_pressed("ui_left"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		direction.z += 1
+	if Input.is_action_pressed("ui_down"):
+		direction.z -= 1
+	direction = direction.normalized()
+	direction = direction * speed * delta
+	
+	velocity.y += gravity * delta
+	velocity.x = direction.x
+	velocity.z = direction.z
+	
+	velocity = move_and_slide(direction, Vector3(0,1,0))
